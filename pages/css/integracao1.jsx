e91ee94f03dfe365e60e74e66d13150b7b5d320b
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-07-07 22:21:38 -0300

    integracao com css

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,11 @@
+
+
+export default function intregacao() {
+    return (
+        <div>
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <duv className="branca">Texto #03</duv>
+        </div>
+    )
+}
