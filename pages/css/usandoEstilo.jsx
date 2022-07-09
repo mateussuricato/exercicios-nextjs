a842fdbec94208b23d7046325edac15c52eb6cf6
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-07-09 14:04:06 -0300

    aplicando estilo css

--- pages/css/usandoEstilo.jsx
+++ pages/css/usandoEstilo.jsx
@@ -1,1 +1,10 @@
-e
+import Estilo from "../../components/estilo";
+
+export default function usandoEstilo() {
+  return (
+    <div>
+      <Estilo numero={3} color="#000"/>
+      <Estilo numero={-13} color="#fff" direita/>
+    </div>
+  )
+}
